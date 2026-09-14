e15f85a5b14b9e7ba0d095ef5743f326de8b2408
Valentine <sirmajed@yandex.ru>
2026-09-14 04:34:34 +0300

    Replace 'err' with 'die' for domain uniqueness check

--- lib/panel/cli.sh
+++ lib/panel/cli.sh
@@ -46,7 +46,7 @@ panel_cli_collect_domains() {
     if [ "$PANEL_DOMAIN" = "$SUB_DOMAIN" ] || \
        [ "$PANEL_DOMAIN" = "$SELFSTEAL_DOMAIN" ] || \
        [ "$SUB_DOMAIN" = "$SELFSTEAL_DOMAIN" ]; then
-        err "Все три домена должны быть уникальными"
+        die "Все три домена должны быть уникальными"
     fi
 }
 
